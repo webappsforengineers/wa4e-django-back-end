5e0257d3cb274990789a4d62615bc15638a9893d
phillybroadbent <phillybroadbent@gmail.com>
2024-01-17 16:50:38 +0000

    Shuffled data and edited model parameters

--- loadAndProcessData.py
+++ loadAndProcessData.py
@@ -9,25 +9,8 @@ from keras import layers, models, optimizers, regularizers
 # Load data from .mat data file
 mat = scipy.io.loadmat('StiffnessNNAppData.mat')
 
-# Examine the keys of the data
-# print(list(mat.keys()))
-
 properties = mat["properties"]
 curves = mat["curves"]
-
-# print(type(properties))
-# print(type(curves))
-
-# np.info(properties)
-# np.info(curves)
-
-# print(properties[0][0][0])
-
-# print(len(properties))
-# print(len(properties[0]))
-# print(len(properties[0][0]))
-# print(len(properties[0][0][0][0]))
-
 
 def preprocess_data(properties, curves):
     processed_data = {'input': np.empty((0, 2)), 'output': np.empty((0, 1))}
@@ -58,52 +41,53 @@ def preprocess_data(properties, curves):
 
 processed_data = preprocess_data(properties, curves)
 
+# shuffle inputs and outputs in unison
+def unison_shuffled_copies(a, b):
+    assert len(a) == len(b)
+    p = np.random.permutation(len(a))
+    return a[p], b[p]
 
 
-inputs_array = pd.DataFrame(processed_data['input'])
+shuffled_data = unison_shuffled_copies(processed_data['input'], processed_data['output'])
+
+inputs_array = pd.DataFrame(shuffled_data[0])
 inputs_array.columns = ['column_of_arrays']
-# print(inputs)
 
 # Function to expand arrays into multiple columns
 def expand_array(row):
     return pd.Series(row['column_of_arrays']) 
 
 inputs_df = inputs_array.apply(expand_array, axis=1)
-# print(inputs_df.shape)
-# print(inputs_df.head())
+
 # Define properties and propSelect
 propSelect = [0, 1, 2, 3, 4, 5, 6, 7]  
 properties = np.array([0.1, 1.019, 1, 0.76, 0.6923, 0.99, 1.2, 161.1])
 layers = [12]
 
-# # Get inputs from processed data
+# Get inputs from processed data
 selected_inputs = inputs_df[np.append(propSelect, 8)]
 
-
 inputs = selected_inputs.to_numpy()
-print(inputs.shape)
-print(inputs)
 
-# # Get outputs from processed data
-targets = processed_data['output']
-print(targets.shape)
-print(targets)
+# Get outputs from processed data
+targets = shuffled_data[1]
 
-
-# # Construct a neural network model
+# Construct a neural network model
 model = tf.keras.Sequential([
-    tf.keras.layers.Dense(units=12, activation='tanh', input_shape=(len(propSelect) + 1,)),
-    # tf.keras.layers.Dropout(0.25), # Adjust dropout rate as needed
+    tf.keras.layers.Dense(units=128, activation='tanh', input_shape=(len(propSelect) + 1,)),
+    tf.keras.layers.Dense(units=128, activation='tanh', input_shape=(len(propSelect) + 1,)),
+    tf.keras.layers.Dense(units=128, activation='tanh', input_shape=(len(propSelect) + 1,)),
+    tf.keras.layers.Dropout(0.6), # Adjust dropout rate as needed
     tf.keras.layers.Dense(units=1, activation='linear')  # Output layer
 ])
 
 # print(model.layers)
 # print(model.summary())
 
-# # # Compile the model
+# Compile the model
 model.compile(optimizer='adam', loss='mse')
 
-# # # Set up Division of Data for Training, Validation, Testing
+# Set up Division of Data for Training, Validation, Testing
 train_ratio = 0.7
 val_ratio = 0.15
 test_ratio = 0.15
@@ -114,27 +98,17 @@ num_val = int(val_ratio * num_samples)
 
 
 x_train, y_train = inputs[0:num_train,:], targets[0:num_train, :]
-# print(type(x_train))
-# print(x_train.shape)
-# print(type(y_train))
-# print(y_train.shape)
-# # print(x_train)
-# # print(y_train)
 x_val, y_val = inputs[num_train:num_train + num_val, :], targets[num_train:num_train + num_val, :]
-# print(x_val.shape)
-# print(type(x_val))
-# print(y_val.shape)
-# print(type(y_val))
-
 x_test, y_test = inputs[num_train + num_val:, :], targets[num_train + num_val:, :]
-# print(x_test.shape)
-# print(y_test.shape)
+
+
+
+# # Train the model
+history = model.fit(x_train, y_train, epochs=200, validation_data=(x_val, y_val), verbose=0)
 
 def plot_loss(history, fig_name):
     plt.figure()
     plt.plot(history.history['loss'], label='loss')
-    plt.plot(history.history['mae'], label='mae')
-    plt.plot(history.history['val_loss'], label='val_loss')
     plt.xlabel('Epoch')
     plt.ylabel('Error')
     plt.legend()
@@ -142,27 +116,13 @@ def plot_loss(history, fig_name):
     plt.savefig(fig_name)
     plt.clf()
 
-# # print(x_train.shape)
-# # print(x_train[0:5])
-# # print(y_train.shape)
-# # print(y_train[0:5])
-
-# # Train the model
-history = model.fit(x_train, y_train, epochs=100, batch_size=32,validation_data=(x_val, y_val), verbose=0)
-print(history.history)
-# # validation_data=(x_val, y_val)
-# # print(history.history)
 
 plot_loss(history, 'plot_loss.png')
 
-# loss, mse = model.evaluate(x_test, y_test)
-# print(f'loss{loss}')
-# print(f'mse{mse}')
+performance = model.evaluate(x_test, y_test)
+print(f'performance: {performance}')
 
 outputs = model.predict(inputs)
-
-print(pd.DataFrame(outputs).describe())
-print(pd.DataFrame(targets).describe())
 
 plt.figure()
 plt.scatter(targets, outputs)
@@ -171,27 +131,6 @@ plt.ylabel('Predictions')
 plt.savefig('targets_vs_outputs.png')
 plt.clf()
 
-# plot_loss(history, 'history.png')
-# # Test the model
-# print("Evaluate on test data")
-# results = model.evaluate(x_test, y_test, batch_size=128)
-# print("test loss, test acc:", results)
-
-
-
-# outputs = model.predict(inputs)
-# performance = model.evaluate(x_test, y_test)
-
-
-# Plot regression
-# plt.figure()
-# plt.plot(targets, outputs, 'o')
-# plt.xlabel('True values')
-# plt.ylabel('Predicted values')
-# plt.title('Regression plot')
-# plt.savefig('regression_plot.png')
-# plt.clf()
-
 # Calculate GGo for different strain values
 strain = np.logspace(-4, 1, 200)
 GGo = []
@@ -199,12 +138,6 @@ GGo = []
 for s in strain:
     inputs_prop = np.append(properties[propSelect], s).reshape(1, -1)
     GGo.append(model.predict(inputs_prop)[0][0])
-
-print(inputs_prop)
-print(inputs_prop)
-print(GGo)
-print(len(strain))
-print(len(GGo))
 
 plt.figure()
 plt.scatter(strain, GGo)
