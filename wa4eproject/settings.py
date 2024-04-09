5120c3ee0d984b94b47e023eeb0712d6008812e0
phillybroadbent <phillybroadbent@gmail.com>
2024-04-09 16:59:59 +0100

    Added configuration for docker volume

--- wa4eproject/settings.py
+++ wa4eproject/settings.py
@@ -86,10 +86,9 @@ WSGI_APPLICATION = 'wa4eproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'data/db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
