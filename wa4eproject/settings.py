ce453b2767446c86f2abea9ce3b6e41c80d80653
phillybroadbent <phillybroadbent@gmail.com>
2024-05-22 12:15:24 +0100

    Adding environment variables for secret key and debug

--- wa4eproject/settings.py
+++ wa4eproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kg2=esjf5!2&jujkb$^+@%n+!j(pfb#r+6fk6on3bgil$=!h22'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool, default=False)
 
 ALLOWED_HOSTS = ['srv04168', 'localhost']
 
